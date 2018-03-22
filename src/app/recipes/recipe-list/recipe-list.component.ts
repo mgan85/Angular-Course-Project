da25b7162b6cb1e6d1a6f6ebe6174876973b19b9
mgan85 <mgan@o2.pl>
2018-03-22 14:16:18 +0000

    fill compontents

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply test','https://cookieandkate.com/images/2017/04/hummus-quesadillas-recipe-1.jpg'),
+    new Recipe('A Test Recipe2', 'This is simply test','https://cookieandkate.com/images/2017/04/hummus-quesadillas-recipe-1.jpg')
+
+  ];
+
 
   constructor() { }
 
